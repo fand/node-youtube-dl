30fd393eff3a839ae869d44abd39223ee0083e20
fand <fand>
2014-05-09 04:11:08 +0900

    added a test for videos on vimeo

--- test/download.js
+++ test/download.js
@@ -5,6 +5,7 @@ var path   = require('path');
 var assert = require('assert');
 var video1  = 'http://www.youtube.com/watch?v=90AiXO1pAiA';
 var video2 = 'https://www.youtube.com/watch?v=179MiZSibco';
+var video3 = 'http://vimeo.com/4992356';
 
 
 vows.describe('download').addBatch({
@@ -71,6 +72,40 @@ vows.describe('download').addBatch({
       if (err) throw err;
       var filepath = path.join(__dirname, '1 1 1-179MiZSibco.en.srt');
       assert.isTrue(fs.existsSync(filepath));
+    }
+  },
+  'a video from vimeo': {
+    topic: function() {
+      var args = [];
+      var dl = ytdl(video3, args, { cwd: __dirname });
+      var cb = this.callback;
+      dl.on('error', cb);
+      dl.on('end', cb);
+      dl.resume();
+    },
+
+    'data returned': function(err, progress, data) {
+      if (err) throw err;
+
+      assert.equal(progress, 1);
+      assert.isObject(data);
+      assert.equal(data.id, '4992356');
+      assert.isTrue(/eap-4992356/.test(data.filename));
+      assert.equal(data.size, 30675);
+    },
+
+    'file was downloaded': function(err, progress, data) {
+      if (err) throw err;
+
+      // Check existance.
+      var filepath = path.join(__dirname, data.filename);
+      var exists = fs.existsSync(filepath);
+      if (exists) {
+        // Delete file after each test.
+        fs.unlinkSync(filepath);
+      } else {
+        assert.isTrue(exists);
+      }
     }
   }
 }).export(module);
